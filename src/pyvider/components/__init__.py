dfd04ae28388cbdf46cc4ddc2f0905c84759316a
Tim <code@tim.life>
2025-09-05 07:58:14 -0700

    🔼⚙️ [skip ci] auto-commit

--- src/pyvider/components/__init__.py
+++ src/pyvider/components/__init__.py
@@ -5,5 +5,10 @@
 # This file makes pyvider.components a package.
 # Autodiscovery will scan from here.
 
+from pyvider.components._version import __version__
+
+__all__ = [
+    "__version__",
+]
 
 # 🧩📦🔍
