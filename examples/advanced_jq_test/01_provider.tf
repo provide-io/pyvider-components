8cd29343a7bae32747efabacb81460fc9ced015b
Tim <code@tim.life>
2025-10-10 16:37:04 -0700

    🔼⚙️ [skip ci] auto-commit

--- examples/advanced_jq_test/01_provider.tf
+++ examples/advanced_jq_test/01_provider.tf
@@ -1,5 +1,5 @@
 terraform {
   #backend "local" { path = ".soup/tfdata/terraform.tfstate" }
-  required_providers { pyvider = { source = "local/providers/pyvider", version = "0.0.6" } }
+  required_providers { pyvider = { source = "local/providers/pyvider", version = "0.1.0" } }
 }
 provider "pyvider" { api_token = "placeholder-token" }
