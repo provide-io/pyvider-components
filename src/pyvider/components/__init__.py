236a337da9859d499f77f48dee34b2317d310a29
Tim <code@tim.life>
2025-10-19 19:47:07 -0700

    🔼⚙️ [skip ci] auto-commit

--- src/pyvider/components/__init__.py
+++ src/pyvider/components/__init__.py
@@ -3,13 +3,12 @@
 # SPDX-FileCopyrightText: Copyright (c) provide.io llc. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-# pyvider/components/__init__.py
-#
-
 # This file makes pyvider.components a package.
 # Autodiscovery will scan from here.
 
-from pyvider.components._version import __version__
+from provide.foundation.utils.versioning import get_version
+
+__version__ = get_version("pyvider-components", caller_file=__file__)
 
 __all__ = [
     "__version__",
