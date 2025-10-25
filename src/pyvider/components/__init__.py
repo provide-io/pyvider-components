22585feaa88fb554139a43743554ff212165191e
Tim <code@tim.life>
2025-10-25 12:48:05 -0700

    🔼⚙️ [skip ci] auto-commit

--- src/pyvider/components/__init__.py
+++ src/pyvider/components/__init__.py
@@ -8,10 +8,16 @@
 
 from provide.foundation.utils.versioning import get_version
 
+# Ensure provider is registered on import
+from pyvider.components.provider import PyviderProvider
+from pyvider.components.capabilities.core import CoreCapability
+
 __version__ = get_version("pyvider-components", caller_file=__file__)
 
 __all__ = [
     "__version__",
+    "PyviderProvider",
+    "CoreCapability",
 ]
 
 # 🧩📦🔍
