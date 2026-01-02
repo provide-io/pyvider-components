f89008c1037a2477c1412efcb65b21caf95d1df3
Tim <code@tim.life>
2026-01-01 20:33:28 -0800

    style: format source files with ruff

--- src/pyvider/components/resources/local_directory.py
+++ src/pyvider/components/resources/local_directory.py
@@ -106,7 +106,9 @@ class LocalDirectoryResource(
         return base_plan, None
 
     @resilient()
-    async def _create_apply(self, ctx: ResourceContext[LocalDirectoryState, None]) -> tuple[LocalDirectoryState | None, None]:
+    async def _create_apply(
+        self, ctx: ResourceContext[LocalDirectoryState, None]
+    ) -> tuple[LocalDirectoryState | None, None]:
         planned_state = cast(LocalDirectoryState, ctx.planned_state)
         path = Path(planned_state.path)
         logger.debug("Creating directory", path=str(path))
@@ -133,7 +135,9 @@ class LocalDirectoryResource(
             ) from e
         return ctx.planned_state, None
 
-    async def _update_apply(self, ctx: ResourceContext[LocalDirectoryState, None]) -> tuple[LocalDirectoryState | None, None]:
+    async def _update_apply(
+        self, ctx: ResourceContext[LocalDirectoryState, None]
+    ) -> tuple[LocalDirectoryState | None, None]:
         return await self._create_apply(ctx)
 
     @resilient()
